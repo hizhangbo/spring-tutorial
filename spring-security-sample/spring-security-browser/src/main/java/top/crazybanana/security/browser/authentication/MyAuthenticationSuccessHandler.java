9f39e84c26260eae9ba465ce3fddb99c5c26a091
hizhangbo <hizhangbo@outlook.com>
2018-12-19 17:56:05 +0800

    登录成功的处理 SavedRequest

--- spring-security-sample/spring-security-browser/src/main/java/top/crazybanana/security/browser/authentication/MyAuthenticationSuccessHandler.java
+++ spring-security-sample/spring-security-browser/src/main/java/top/crazybanana/security/browser/authentication/MyAuthenticationSuccessHandler.java
@@ -4,7 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
@@ -23,9 +28,22 @@ public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHand
     @Autowired
     private ObjectMapper objectMapper;
 
+    private RequestCache requestCache = new HttpSessionRequestCache();
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         log.info("登录成功：{}", authentication.getDetails());
+
+        SavedRequest savedRequest = requestCache.getRequest(request, response);
+
+        if (savedRequest != null) {
+            String targetUrl = savedRequest.getRedirectUrl();
+            log.info("登录成功，跳转到最初始访问的页面：{}", targetUrl);
+            redirectStrategy.sendRedirect(request, response, targetUrl);
+            return;
+        }
 
         response.setContentType("application/json;charset=UTF-8");
         response.getWriter().write(objectMapper.writeValueAsString(authentication));
